65a149f43e544605edbcaacae7ef20df2388f0fe
BrenoAndradedemoura <brenomourasandrade@gmail.com>
2023-03-27 10:23:11 -0300

    tudo certinho

---
+++ src/screens/Slider3/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FlatList, ImageBackground, View } from 'react-native';
+import { IPage } from '../../../App';
+import {
+    ComponentButtonSlider, ComponentListMarker, ComponentTitleSlider
+} from '../../components';
+import { styles } from './styles';
+export function Slider3({ setPageI }: IPage){
+    const slide1 = require("../../assets/slide1.png")
+    const slide1Texts = [
+        { id: '1', text: 'Atenda o cliente mais próximo' },
+        { id: '2', text: 'Escolha o motorista a ser acionado' },
+        { id: '3', text: 'Considere o trânsito nas vias' }
+    ]
+    return (
+        <View style={styles.container} >
+           <View style={styles.panel}>
+                <ComponentTitleSlider titleI='Operação Eficiente' />
+                <FlatList
+                    data={slide1Texts}
+                    renderItem={({ item }) =>
+                       <ComponentListMarker key={item.id} textMarker={item.text} />
+                    }
+                    keyExtractor={(item) => item.id}
+                />
+            </View>
+            <View style={styles.buttonSlider}>
+                <ComponentButtonSlider onPressI={() => setPageI(1)} />
+                <ComponentButtonSlider onPressI={() => setPageI(2)} />
+                <ComponentButtonSlider onPressI={() => setPageI(3)} />
+                <ComponentButtonSlider onPressI={() => setPageI(4)} />
+            </View>
+        </View>
+    );
+}
---
+++ src/styles/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+    primary: '#E2AF2B',
+    secondary: '#FFEE51',
+    third: '#D9D9D9',
+    thirdLight: 'rgba(217,217,217, 0.8)',
+    black: '#000',
+    white: '#FFF'
+}
